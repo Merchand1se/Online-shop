3e92b3a5ed7386dd9bea7034948dcf0daf41b7d6
Merchand1se <157629323+Merchand1se@users.noreply.github.com>
2024-05-22 22:01:00 +0700

    Разработка почтового сервиса

---
+++ Django D3/Practice/simpleapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+# Импортируем созданное нами представление
+from .views import ProductsList, ProductDetail, create_product, ProductUpdate, ProductDelete
+
+
+urlpatterns = [
+   path('', ProductsList.as_view(), name='product_list'),
+   path('<int:pk>', ProductDetail.as_view(), name='product_detail'),
+   path('create/', create_product.as_view(), name='product_create'),
+   path('<int:pk>/update/', ProductUpdate.as_view(), name='product_update'),
+   path('<int:pk>/delete/', ProductDelete.as_view(), name='product_delete'),
+]
+
+
